a389723b81663502984cadda3c79c38edecf2cc4
elib00 <joshuanapinas19@gmail.com>
2024-05-02 23:39:31 +0800

    [init][Joshua] Initialized local folder

--- app/src/main/java/com/example/vulqit/MainActivity.java
+++ app/src/main/java/com/example/vulqit/MainActivity.java
@@ -1,16 +1,54 @@
 package com.example.vulqit;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.MenuItem;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.material.badge.BadgeDrawable;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    HomeFragment homeFragment = new HomeFragment();
+    ShopsFragment shopsFragment = new ShopsFragment();
+    MapFragment mapFragment = new MapFragment();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        Toast.makeText(MainActivity.this, "Welcome, User!", Toast.LENGTH_SHORT).show();
+
+        initializeView();
+    }
+
+    private void initializeView(){
+        bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavContainer);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, homeFragment).commit();
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                if(item.getItemId() == R.id.home){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, homeFragment).commit();
+                }else if(item.getItemId() == R.id.map){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, mapFragment).commit();
+                }else if(item.getItemId() == R.id.shops){
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, shopsFragment).commit();
+                }
+                
+                
+                return true;
+            }
+        });
     }
 }
--- app/src/main/java/com/example/vulqit/SplashActivity.java
+++ app/src/main/java/com/example/vulqit/SplashActivity.java
@@ -5,7 +5,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
-import android.widget.Toast;
 
 public class SplashActivity extends AppCompatActivity {
 
