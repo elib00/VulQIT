a3f92f3094f21c44cfab509d992a2c8b3668fd51
elib00 <joshuanapinas19@gmail.com>
2024-05-11 21:29:26 +0800

    [Joshua] Final changes

--- app/src/main/java/com/example/vulqit/MainActivity.java
+++ app/src/main/java/com/example/vulqit/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vulqit;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
@@ -7,7 +8,9 @@ import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -31,6 +34,37 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         initializeView();
     }
+
+//    @Override
+//    public void onBackPressed() {
+//        FragmentManager fragmentManager = getSupportFragmentManager();
+//        if (fragmentManager.getBackStackEntryCount() >= 0) {
+//            super.onBackPressed();
+//        } else {
+//            showExitConfirmationDialog();
+//        }
+//    }
+//
+//    private void showExitConfirmationDialog() {
+//        AlertDialog.Builder alertDialog = new AlertDialog.Builder(MainActivity.this);
+//        alertDialog.setTitle("Exit VulQIT");
+//        alertDialog.setMessage("Have you located your shop?");
+//        alertDialog.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                finishAffinity();
+//            }
+//        });
+//        alertDialog.setNegativeButton("No", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialogInterface, int i) {
+//                dialogInterface.dismiss();
+//            }
+//        });
+//        alertDialog.show();
+//    }
+
+
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -61,13 +95,13 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                 if(item.getItemId() == R.id.home){
                     System.out.println("home ni");
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, homeFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, homeFragment).addToBackStack(null).commit();
                 }else if(item.getItemId() == R.id.map){
                     System.out.println("maps ni");
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, mapFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, mapFragment).addToBackStack(null).commit();
                 }else if(item.getItemId() == R.id.shops){
                     System.out.println("shops ni");
-                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, shopsFragment).commit();
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragmentsContainer, shopsFragment).addToBackStack(null).commit();
                 }
 
 
--- app/src/main/java/com/example/vulqit/ShopsFragment.java
+++ app/src/main/java/com/example/vulqit/ShopsFragment.java
@@ -85,9 +85,8 @@ public class ShopsFragment extends Fragment {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
-
-
     }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
